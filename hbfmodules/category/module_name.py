3082175ccb5513e54d024b08b033fae0c19da740
Jordan Ovrè <ovre.jordan@gmail.com>
2019-09-13 14:48:00 +0200

    Add hbf_config to init call

--- hbfmodules/category/module_name.py
+++ hbfmodules/category/module_name.py
@@ -2,8 +2,8 @@ from hydrabus_framework.modules.AModule import AModule
 
 
 class ClassName(AModule):
-    def __init__(self):
-        super(ClassName, self).__init__()
+    def __init__(self, hbf_module):
+        super(ClassName, self).__init__(hbf_module)
         self.meta.update({
             'name': '',
             'version': '',
